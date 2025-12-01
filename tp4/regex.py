4706dff4192ec270434c87359f1d4fa6dea258bc
ayoubbezai <bezaiayoub8@gmail.com>
2025-12-02 00:22:41 +0100

    extract years

--- tp4/regex.py
+++ tp4/regex.py
@@ -1,5 +1,8 @@
+from typing import List
+import re
 def readTheBook(fileName : str) -> str:
-    """ Read and return the file content 
+    """ 
+    Read and return the file content 
         Arguments:
             fileName : str : the name of the file to read
         Returns:
@@ -13,6 +16,29 @@ def readTheBook(fileName : str) -> str:
     except :
         print("file not found")
         return ""
+    
+    
+def extractYears(content : str) -> List[str] :
+    """
+    Extract years from the book 
+        Arguments : 
+            content : str : the text after read it int string
+        Return :
+            years : List[str] : return all the years as  list of strings 
+        Exemple 
+            years = extractYears(content)
+            print(years)
+            //output
+            [2012,1039]
+    """
+    pattren = r"\d{4}"
+    years = re.findall(pattren,content)
+    return years
+    
+
+
 
 if __name__ == "__main__":
-    readTheBook('tp4/the-prince.txt')
+    content = readTheBook('tp4/the-prince.txt')
+    years = extractYears(content)
+    print(years)
